bf75c29633793b3847ea19eb97ea0fcb2cce5dfa
Alfian Reno Prasetyo <70306653+AlfianReno@users.noreply.github.com>
2022-02-17 15:39:10 +0700

    Add files via upload

---
+++ CRUD-AJAX/php/addData.php
@@ -0,0 +1,16 @@
+<?php
+    include('connection.php');
+    $idkategori = $_POST['idkategori'];
+    $menu = $_POST['menu'];
+    $gambar = $_POST['gambar'];
+    $harga = $_POST['harga'];
+    $sql = mysqli_query($connection, "INSERT INTO tblmenu VALUES (null,'$idkategori','$menu','$gambar','$harga')" );
+        if($sql) {
+            $result['status'] = '1';
+            $result['msg'] = 'Insert data berhasil';
+        } else {
+            $result['status'] = '0';
+            $result['msg'] = 'Insert data gagal!';
+        }
+        echo json_encode($result);
+?>
---
+++ CRUD-AJAX/php/connection.php
@@ -0,0 +1,6 @@
+<?php 
+$connection = mysqli_connect('localhost', 'root', '', 'dbrestoran');
+if (mysqli_connect_errno()) {
+    echo "Koneksi Database Gagal!". mysqli_connection_error();
+}
+?>
